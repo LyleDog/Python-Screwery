66ec85d6ed6509bc1b3689611d785e29d75ce9ec
LyleDog <lloyd.faraday@gmail.com>
2017-02-07 09:20:05 +0900

    os.path has been used to set chdir!!

--- ModuleTest.py
+++ ModuleTest.py
@@ -3,14 +3,19 @@ from TerminalGraphicsModule import Screen
 from TerminalGraphicsModule import Render
 from TerminalGraphicsModule import World
 
+##Obtaining the path of the script
+dirPath = os.path.dirname(os.path.abspath(__file__))
+
 screen = Screen.start()
 Screen.printScreen(screen)
 input("Cleared?\n")
 os.system("cls")
 input("Starting World and Render tests...\n")
-os.chdir("C:\\Users\\Batarda\\Desktop\\Python-Screwery\\TerminalGraphicsModule")
 world = {}
-world = World.loadModel("Model.txt", world)
+##applying dirpath to chdir
+os.chdir(dirPath)
+
+world = World.loadModel("TerminalGraphicsModule\\Model.txt", world)
 World.printWorld(world)
 input("Cleared?\n")
 os.system("cls")
