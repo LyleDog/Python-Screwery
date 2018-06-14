e699ffb3781f2ef51777ae975b3dc27d5f68d3f4
Lyle F <lloyd.faraday@gmail.com>
2018-06-14 03:28:20 -0400

    Update Square Root Calculator.py

--- Square Root Calculator.py
+++ Square Root Calculator.py
@@ -1,9 +1,11 @@
-print ("A stupid fucking square root calculator.")
+# 2018 comments
+
+print ("A stupid square root calculator.")
 var1 = float(input("Value?\n"))
-i = 1
+i = 1 # this hurts my soul
 guessnumber = 0
-approach = 0
-while True :
+approach = 0 # What was I thinking, I'm using integer truthiness in this scenario, just very very wrongly.
+while True : # What is this C or something
 	guess = i*i
 	if (guess == var1) :
 		break
@@ -19,3 +21,5 @@ while True :
 		break
 print ("The square root of", var1, "is approximately", round(i,4))
 input("Thank you.")
+
+# Overall the algorithm I chose to do this isn't HALF bad, but the implementation just shows that I only knew some basic C at the time.
